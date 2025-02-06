58cfd7f3b737c08f5499b7872e1f6b338b216715
sheharaR <shalikaramanayaka21@gmail.com>
2025-02-06 14:28:37 +0530

    settings

--- chattermind/chattermind/urls.py
+++ chattermind/chattermind/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
--- chattermind/webapp/urls.py
+++ chattermind/webapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('settings/', views.settings, name='settings'),  # New settings page
+    path('update-profile/', views.update_profile, name='update_profile'),  # For updating user info
+    path('toggle-theme/', views.toggle_theme, name='toggle_theme'),  # For toggling dark mode
 ]
